ef5937922b2eced8336094d03b7a919641bd7b2a
RAGNHILD93 <43165582+RAGNHILD93@users.noreply.github.com>
2018-10-11 14:33:15 +0200

    R-skript

---
+++ Impatiens script.R
@@ -0,0 +1,8 @@
+#### Semesteroppgave ####
+
+rm(list=ls())
+search()
+
+Impatiens <- read.table("/Users/raggen93/Documents/Google Drive/Skole/BI3081 Quantitative analyses/Semesteroppgave/data_impatiens.txt", header=T)
+
+head(Impatiens)
